e32e72dbdb7de9c6a49c92a1484024433f719220
Yan Shcherbakov <yanshcherbakov@Yans-MacBook-Pro-2.local>
2013-07-01 12:10:48 -0400

    Initial Commit

---
+++ Pebble Adventure Navigation/PANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PANAppDelegate.h
+//  Pebble Adventure Navigation
+//
+//  Created by Yan Shcherbakov on 2013-07-01.
+//  Copyright (c) 2013 One Shot Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pebble Adventure Navigation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pebble Adventure Navigation
+//
+//  Created by Yan Shcherbakov on 2013-07-01.
+//  Copyright (c) 2013 One Shot Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PANAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PANAppDelegate class]));
+    }
+}
