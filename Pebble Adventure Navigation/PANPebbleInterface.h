8512b4cfc58a893bbdf4eb7d41fed66b14e9f193
Yan Shcherbakov <yanshcherbakov@Yans-MacBook-Pro-2.local>
2013-07-17 23:05:23 -0400

    Changed.

---
+++ Pebble Adventure Navigation/PANPebbleInterface.h
@@ -0,0 +1,13 @@
+//
+//  PANPebbleInterface.h
+//  Pebble Adventure Navigation
+//
+//  Created by Yan Shcherbakov on 2013-07-08.
+//  Copyright (c) 2013 One Shot Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PANPebbleInterface : NSObject
+
+@end
---
+++ Pebble Adventure Navigation/PANPebbleInterface.m
@@ -0,0 +1,13 @@
+//
+//  PANPebbleInterface.m
+//  Pebble Adventure Navigation
+//
+//  Created by Yan Shcherbakov on 2013-07-08.
+//  Copyright (c) 2013 One Shot Apps. All rights reserved.
+//
+
+#import "PANPebbleInterface.h"
+
+@implementation PANPebbleInterface
+
+@end
